af1439fe254938bd5ca82a00fcf5afa52c0c9fef
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-08-15 17:21:53 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Mvc; //added in here.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAppSchedule
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            FluentValidationModelValidatorProvider.Configure();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
